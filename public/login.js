99f85b2376c903a0443a15066f2f82c15dbe8083
connor-r78 <connorrakov@gmail.com>
2025-12-13 12:06:55 -0500

    I've never been happier

--- public/login.js
+++ public/login.js
@@ -1,6 +1,6 @@
 async function loginAndFetch()
 {
-  const response = await fetch("https://honest-sloths-allow.loca.lt", {
+  const response = await fetch("/api", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
